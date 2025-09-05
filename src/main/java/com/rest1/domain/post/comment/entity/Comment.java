16d89607973134c033fbdc5a38b8c4af7411d17c
caketower <dlaqudtn1107@naver.com>
2025-09-05 15:26:00 +0900

    [5강 ] Post와 Comment간 순환 참조 발생. @JsonIgnore를 이용해 순환 참조 해결.

--- src/main/java/com/rest1/domain/post/comment/entity/Comment.java
+++ src/main/java/com/rest1/domain/post/comment/entity/Comment.java
@@ -1,5 +1,6 @@
 package com.rest1.domain.post.comment.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.rest1.domain.post.post.entity.Post;
 import com.rest1.global.jpa.entity.BaseEntity;
 import jakarta.persistence.Entity;
@@ -16,6 +17,8 @@ public class Comment extends BaseEntity {
 
     private String content;
     @ManyToOne
+    //@Getter(AccessLevel.PRIVATE)    // 외부에서 접근 불가 -> JSON에서 데이터를 뽑을때 content는 뽑을수 있지만 post는 뽑을수 없게됨
+    @JsonIgnore // JSON에서 데이터를 뽑을때 이건 무시하라는 이야기 -> @Getter(AccessLevel.PRIVATE)과 같은 효과인데 comment.getpost()는 가능해짐
     private Post post;
 
     public void update(String content) {
--- src/main/java/com/rest1/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/rest1/domain/post/post/controller/ApiV1PostController.java
@@ -6,20 +6,25 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 //@RestController -> 이걸 쓰면 @ResponseBody를 생략가능
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/posts")
 public class ApiV1PostController {
     private final PostService postService;
 
-    @GetMapping("/api/v1/posts")
+    @GetMapping
     @Transactional(readOnly = true)
     @ResponseBody
     public List<Post> list() {
         //더이상 templates방식을 쓰지 않는다.
-        return  postService.findAll();
+        List<Post> posts = postService.findAll();
+        System.out.println("hihihi");
+
+        return posts;
     }
 }
