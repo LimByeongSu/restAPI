4334f61e95a108822d24efba9c4c64977263ea92
caketower <dlaqudtn1107@naver.com>
2025-09-08 12:25:59 +0900

    [14강]  resultCode와 msg는 조회를 제외한 모든 요청에 대한 응답의 필수요소이기 때문에 RsData 클래스 도입

---
+++ src/main/java/com/rest1/global/rsData/RsData.java
@@ -0,0 +1,14 @@
+package com.rest1.global.rsData;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class RsData {   //resultData라는 의미다.
+
+    private String resultCode;
+    private String msg; //메세지
+
+
+}
